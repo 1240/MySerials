fe3c542fcc50c7b37d36ffd1f9ea9ab8595870b4
l24o <chuffey.1240@yandex.ru>
2016-03-18 14:44:44 +0300

    add episode

---
+++ app/src/main/java/com/l24o/myserials/models/Episode.java
@@ -0,0 +1,58 @@
+package com.l24o.myserials.models;
+
+import java.util.Date;
+
+import io.realm.RealmList;
+import io.realm.RealmObject;
+
+/**
+ * Created by l24o on 18.03.16.
+ */
+public class Episode extends RealmObject {
+
+    private String name;
+    private String code;
+    private String rating;
+    private Date date;
+    private RealmList<TranslateTeam> translateTeams;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public RealmList<TranslateTeam> getTranslateTeams() {
+        return translateTeams;
+    }
+
+    public void setTranslateTeams(RealmList<TranslateTeam> translateTeams) {
+        this.translateTeams = translateTeams;
+    }
+}
